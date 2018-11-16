f8f99819fa9fbc60cdf254ab359f9e7f66cf95d0
BT_1N3_06@bktech.local <BT_1N3_06@bktech.local>
2018-11-16 10:17:17 -0500

    Initial commit

---
+++ src/InPlaceSorts.java
@@ -0,0 +1,59 @@
+public class InPlaceSorts
+{
+    public static void insertionSort(int[] list1)
+    {
+        int swapPosition = 0;
+        int min = 0;
+        for(int i = 1; i < list1.length; i++)
+        {
+            swapPosition = i;
+            min = list1[i];
+            for(int x = i-1; x>0; x--)
+            {
+                if(list1[x]>min)
+                {
+                    int temp = min;
+                    min = list1[x];
+                    list1[x] = temp;
+                    swapPosition = x;
+                }
+            }
+        }
+    }
+    public static void selectionSort(double[] list1)
+    {
+        int swapPosition = 0;
+        double min = 0;
+        for(int i = 0; i < list1.length; i++)
+        {
+            swapPosition = i;
+            min = list1[i];
+            for(int x = i+1; x< list1.length; x++)
+            {
+                if(list1[x]<min)
+                {
+                    double temp = min;
+                    min = list1[x];
+                    list1[x] = temp;
+                    swapPosition = x;
+                }
+            }
+        }
+    }
+    public static void bubbleSort(String[] list1)
+    {
+        int swap = 0;
+        while(swap!=0)
+        {
+            swap = 0;
+            for(int i = 0; i< list1.length -1; i++)
+            {
+                swap++;
+                String temp = list1[i];
+                list1[i] = list1[i+1];
+                list1[i+1] = temp;
+            }
+        }
+    }
+
+}
